039c922a13a865fff2a68e6e61842c3652212fbb
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-10-06 22:25:17 +0300

    lab08a

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -6,9 +6,23 @@ package quotes;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Quotes q = new Quotes();
-        System.out.println(q.readFromJsonFile("app/src/main/resources/recentquotes.json"));
-        System.out.println(q.readFromJsonFile("app/src/main/resources/testQuote.json"));
+//        Quotes q = new Quotes();
+//        System.out.println(q.readFromJsonFile("app/src/main/resources/recentquotes.json"));
+//        System.out.println(q.readFromJsonFile("app/src/main/resources/testQuote.json"));
+        String filePath = "./app/src/main/resources/recentquotes.json";
+//        String uRLPath = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+        try
+        {
+
+            System.out.println(Quotes.readFromAPI(filePath));
+
+        }
+        catch (Exception error)
+        {
+            System.err.println(error);
+        }
+
 
 
     }
--- app/src/main/java/quotes/Quotes.java
+++ app/src/main/java/quotes/Quotes.java
@@ -3,18 +3,20 @@ package quotes;
 import java.io.*;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Quotes {
     private String author;
     private String text;
 
-//    public Quotes(String author, String text) {
-//        this.author = author;
-//        this.text = text;
-//    }
+    public Quotes(String text) {
+        this.text = text;
+        this.author = "ron-swanson";
+    }
 
     public Quotes() {
 
@@ -35,6 +37,76 @@ public class Quotes {
 
     public String getText() {
         return text;
+    }
+    public static Quotes readFromAPI( String backupFile)
+    {
+        String urlPath = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+        StringBuilder content = new StringBuilder();
+        Quotes quote = null;
+
+        try
+        {
+
+            URL url = new URL(urlPath);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+//            System.out.println(connection.getResponseCode());
+
+            // synchronous: Java is going to be working on running line 15 for a while.
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = input.readLine()) != null) {
+                content.append(inputLine);
+            }
+            input.close();
+
+            quote = new Quotes(content.toString().replaceAll("]$|^\\[",""));
+
+            try
+            {
+                fileWriter(backupFile, quote);
+            }
+            catch (Exception errorFile)
+            {
+                System.err.println(errorFile);
+            }
+        }
+        catch (IOException errorAPI)
+        {
+
+            System.err.println("No internet connection.");
+            try
+            {
+                quote = new Quotes(readFromJsonFile(backupFile));
+            }
+            catch (Exception error)
+            {
+                System.err.println(error);
+            }
+        }
+
+        return quote;
+    }
+
+    public static void fileWriter(String filePath, Quotes quote) throws Exception
+    {
+        Gson gson = new Gson();
+
+        BufferedReader file = new BufferedReader(new FileReader(filePath));
+
+        TypeToken<ArrayList<Quotes>> token = new TypeToken<ArrayList<Quotes>>(){};
+
+        ArrayList<Quotes> quotesFromFiles = gson.fromJson(file, token.getType());
+
+        quotesFromFiles.add(quote);
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+
+        writer.write(gson.toJson(quotesFromFiles));
+
+        writer.close();
+        file.close();
     }
 
 
